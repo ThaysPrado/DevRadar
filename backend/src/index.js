37fc57fb641360b76449479097ca813f25002528
ThaysPrado <thaysprado96@gmail.com>
2020-04-05 14:04:06 -0300

    init project, routes: create, list and search devs

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(process.env.URL_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
