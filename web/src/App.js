c9e0a3f2ee1afedc3e40d85f1b56aa088d10b57b
ThaysPrado <thaysprado96@gmail.com>
2020-04-05 23:39:57 -0300

    web project - dashboard static

---
+++ web/src/App.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img  src="https://avatars2.githubusercontent.com/u/8344547?v=4" alt="ThaysPrado" />
+              <div className="user-info">
+                <strong>Thays Prado</strong>
+                <span>React, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img  src="https://avatars2.githubusercontent.com/u/8344547?v=4" alt="ThaysPrado" />
+              <div className="user-info">
+                <strong>Thays Prado</strong>
+                <span>React, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img  src="https://avatars2.githubusercontent.com/u/8344547?v=4" alt="ThaysPrado" />
+              <div className="user-info">
+                <strong>Thays Prado</strong>
+                <span>React, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img  src="https://avatars2.githubusercontent.com/u/8344547?v=4" alt="ThaysPrado" />
+              <div className="user-info">
+                <strong>Thays Prado</strong>
+                <span>React, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
